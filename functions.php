179baa8a9bd38eb8536749f72034478120627624
kirandash <thekirankumardash@gmail.com>
2016-08-03 07:43:58 +0530

    11. Adding Our Submenus

--- functions.php
+++ functions.php
@@ -14,12 +14,32 @@ function kd_add_options_page(){
 		'Theme Options',			// The text to be displayed in the browser title bar
 		'Theme Options',			// The text to be used for the menu
 		'manage_options',			// The required capability of users to access this menu
-		'kd-theme-options',			// The slud by which the menu item is accessible
+		'kd-theme-options',			// The slug by which the menu item is accessible
 		'kd_theme_options_display',	// The name of the cb function used to display the page content
 		''							// Provides a default icon for our menu page
 		//11,						// Avoid using this as there is potential chance of replacing other menu
 		//https://wordpress.org/support/topic/custom-post-type-menu-positions
 	);
+	
+	// Introduce a submenu page specifically for the header options
+	add_submenu_page(
+		'kd-theme-options',					// The slug for the parent menu page to which this sub menu belongs
+		'Header Options',					// The text that's rendered in the browser title bar
+		'Header Options',					// The text to be rendered in the menu
+		'manage_options',					// The required capability of users to access this menu
+		'header-options',					// The slug by which the sub menu is identified
+		'kd_theme_header_options_display'	//The function used to display the options for this menu page
+	);
+	
+	// Introduce a submenu page specifically for the footer options
+	add_submenu_page(
+		'kd-theme-options',					// The slug for the parent menu page to which this sub menu belongs
+		'Footer Options',					// The text that's rendered in the browser title bar
+		'Footer Options',					// The text to be rendered in the menu
+		'manage_options',					// The required capability of users to access this menu
+		'footer-options',					// The slug by which the sub menu is identified
+		'kd_theme_footer_options_display'	//The function used to display the options for this menu page
+	);	
 }
 
 add_action( 'admin_menu', 'kd_add_options_page' );
@@ -35,18 +55,18 @@ add_action( 'admin_menu', 'kd_add_options_page' );
 function kd_initialize_theme_options(){
 	
 	add_settings_section(
-		'options_section',							// The ID to use for this section in attribute tags
+		'header_section',							// The ID to use for this section in attribute tags
 		'Header Options',							// The title of the section rendered to the screen
 		'kd_theme_header_description_display',		// The function used to render the options for this section
-		'kd-theme-header-options'							// The ID of the page on which the section is rendered		
+		'kd-theme-header-options'					// The ID of the page on which the section is rendered		
 	);
 	
 	// Let's introduce a section to be rendered on the new options page
 	add_settings_section(
-		'options_section',				// The ID to use for this section in attribute tags
+		'footer_section',				// The ID to use for this section in attribute tags
 		'Footer Options',				// The title of the section rendered to the screen
 		'kd_footer_options_display',	// The function used to render the options for this section
-		'kd-theme-footer-options'				// The ID of the page on which the section is rendered
+		'kd-theme-footer-options'		// The ID of the page on which the section is rendered
 	);	
 
 	add_settings_field(
@@ -54,7 +74,7 @@ function kd_initialize_theme_options(){
 		'Display Header Text',			// The text used to label the field
 		'kd_header_text_display',		// The cb fn used to render the field
 		'kd-theme-header-options',		// The page on which we'll be rendering this field
-		'options_section'				// The section to which we are adding the field
+		'header_section'				// The section to which we are adding the field
 	);
 		
 	// Define the settings field
@@ -62,18 +82,18 @@ function kd_initialize_theme_options(){
 		'footer_message',				// The ID (or name) of the field
 		'Theme Footer Message',			// The text used to label the field
 		'kd_footer_message_display',	// The cb fn used to render the field
-		'kd-theme-footer-options',				// The page on which we'll be rendering this field
-		'options_section'				// The section to which we are adding the field
+		'kd-theme-footer-options',		// The page on which we'll be rendering this field
+		'footer_section'				// The section to which we are adding the field
 	);
 
 	register_setting(
-		'options_section',				// The name of the group of the settings
+		'header_section',				// The name of the group of the settings
 		'header_options'				// The name of the actual option (or setting)
 	);
 		
 	// Register the 'footer_message' setting with the 'General' section
 	register_setting(
-		'options_section',				// The name of the group of the settings
+		'footer_section',				// The name of the group of the settings
 		'footer_options'				// The name of the actual option (or setting)
 	);
 } // end kd_initialize_theme_options
@@ -84,6 +104,9 @@ add_action('admin_init','kd_initialize_theme_options');
  * Callbacks
 /*----------------------------------------------*/
 
+/**
+ * Renders the content of the options page for both the header and footer options
+ */
 function kd_theme_options_display(){
 ?>
 	<div class="wrap">
@@ -92,7 +115,8 @@ function kd_theme_options_display(){
         	<?php
 				
 				// Renders the settings for the settings section identified as 'Footer Section'
-				settings_fields('options_section');
+				settings_fields('header_section');
+				settings_fields('footer_section');
 				
 				// Renders all of the settings for 'kd-theme-options' section
 				do_settings_sections('kd-theme-header-options');
@@ -105,6 +129,54 @@ function kd_theme_options_display(){
     </div><!-- .wrap -->
 <?php
 } // end kd_theme_options_display
+
+/**
+ * Renders the content of the options page for the header options
+ */
+function kd_theme_header_options_display(){
+?>
+	<div class="wrap">
+        <h2>Kiran Theme Options</h2>
+        <form method="post" action="options.php">
+        	<?php
+				
+				// Renders the settings for the settings section identified as 'Header Section'
+				settings_fields('header_section');
+				
+				// Renders all of the settings for 'header-options' section
+				do_settings_sections('kd-theme-header-options');
+				
+				// Add the submit button to serialize the options
+				submit_button();
+			?>
+        </form>
+    </div><!-- .wrap -->
+<?php
+} // end kd_theme_header_options_display
+
+/**
+ * Renders the content of the options page for the footer options
+ */
+function kd_theme_footer_options_display(){
+?>
+	<div class="wrap">
+        <h2>Kiran Theme Options</h2>
+        <form method="post" action="options.php">
+        	<?php
+				
+				// Renders the settings for the settings section identified as 'Header Section'
+				settings_fields('footer_section');
+				
+				// Renders all of the settings for 'footer-options' section
+				do_settings_sections('kd-theme-footer-options');
+				
+				// Add the submit button to serialize the options
+				submit_button();
+			?>
+        </form>
+    </div><!-- .wrap -->
+<?php
+} // end kd_theme_footer_options_display
 
 /**
  * Renders the description of the settings below the title of the header section
